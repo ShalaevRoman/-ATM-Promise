e7d191309e04241ad695570a1e166cea0d06cbd0
ShalaevRoman <mc.shel24@mail.com>
2021-09-21 12:44:40 +0300

    start process

---
+++ script.js
@@ -0,0 +1,33 @@
+let userData = {
+  'USD': 1000,
+  'EUR': 900,
+  'UAH': 15000,
+  'BIF': 20000,
+  'AOA': 100
+},
+bankData = {
+  'USD': {
+    max: 3000,
+    min: 100,
+    img: '💵'
+  },
+  'EUR': {
+    max: 1000,
+    min: 50,
+    img: '💶'
+  },
+  'UAH': {
+    max: 0,
+    min: 0,
+    img: '💴'
+  },
+  'GBP': {
+    max: 10000,
+    min: 100,
+    img: '💷'
+  }
+}
+
+function getMoney(userData, bankData) {
+  return new Promise((resolve, reject))
+}
